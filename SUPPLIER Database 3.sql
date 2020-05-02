36038b0a9903e86c3fdbb006c28ddb97359d22b8
hemanthkumar7 <53857478+hemanthkumar7@users.noreply.github.com>
2020-05-02 13:57:57 +0530

    Update and rename HEMANTH3 SUPPLIER.sql to SUPPLIER Database 3.sql

--- SUPPLIER Database 3.sql
+++ SUPPLIER Database 3.sql
@@ -38,17 +38,36 @@ insert into catalog values(10004,20003,40);
 select * from catalog;
 select * from suppliers;
 select * from Parts;
+
+Queries:
+/*find the 'sid's of suppliers who supply some red or green parts */
 select distinct(suppliers.sid) from Suppliers,Parts,catalog 
 where Suppliers.sid = catalog.sid and parts.pid=catalog.pid and Parts.color in("Red","Green");
 
+/* find the 'sid's of suppliers who supply some red parts or whose city is bangalore */
 select distinct(suppliers.sid) from Suppliers,Parts,catalog 
 where Suppliers.sid = catalog.sid and parts.pid=catalog.pid and Parts.color = "Red" or Suppliers.city ="Bangalore";
 
+/* find pairs of 'sid's such that the suppliers with the first sid charges more for some part than the supplier with the second sid */
 select s1.sid,s2.sid,p1.pid from Suppliers s1,Suppliers s2, Parts p1, catalog c1,catalog c2
 where s1.sid =c1.sid and s2.sid=c2.sid and p1.pid=c1.pid and p1.pid= c2.pid and c1.cost>c2.cost;
 
-//nested query
-select s.sname,p.pname
-from suppliers s,parts p, catalog c
-where s.sid=c.sid and p.pid=c.pid and c.cost=(select max(cost) from catalog 
-     where p.pid=pid);
+/*Find the sids of suppliers who charge more for some part than the average cost of that part 
+(averaged over all the suppliers who supply that part). */
+
+SELECT DISTINCT C.sid FROM Catalog C
+ WHERE C.cost > ( SELECT AVG (C1.cost)
+  FROM Catalog C1
+ WHERE C1.pid = C.pid );
+ 
+ /*For each part, find the sname of the supplier who charges the most for that part. */
+
+SELECT P.pid, S.sname
+FROM Parts P, Suppliers S, Catalog C
+WHERE C.pid = P.pid
+AND C.sid = S.sid
+AND C.cost = (SELECT MAX (C1.cost)
+		FROM Catalog C1
+		WHERE C1.pid = P.pid);
+
+
